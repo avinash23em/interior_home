5077c4280da860cff31f1d9532e1f9f0049c3920
avinash23em <ap23vi@gmail.com>
2025-03-25 21:21:52 +0530

    intial

---
+++ src/components/navbar.jsx
@@ -0,0 +1,48 @@
+import Logo from "../assets/Logo(1).png";
+const NavLinks = [
+  {
+    id: 1,
+    title: "About",
+    link: "#",
+  },
+  {
+    id: 2,
+    title: "Services",
+    link: "#",
+  },
+  {
+    id: 3,
+    title: "Project",
+    link: "#",
+  },
+  {
+    id: 4,
+    title: "Contact",
+    link: "#",
+  },
+];
+export default function Navbar() {
+  return (
+    <div className="container py-4 flex justify-between items-center">
+      <div className="flex items-center gap-3 p-4">
+        <img src={Logo} alt="logo" className="w-10" />
+        <span className="text-2xl font-bold">INTERIOR</span>
+      </div>
+      <div className="hidden md:block !space-x-12 flex justif-between items-center">
+        {NavLinks.map((link) => {
+          return (
+            <a
+              href={link.link}
+              className="inline-block mx-4 text-lg font-semibold"
+            >
+              {link.title}
+            </a>
+          );
+        })}
+      </div>
+      <div>
+     <button className="text-sm lg:text-base border-2 border-black px-4 py-2 shadow-[5px_5px_0px_0px]">Try for free</button>
+      </div>
+    </div>
+  );
+}
