98b4e49f8cc58115d59e66c87d107fed7d8e1469
avinash23em <ap23vi@gmail.com>
2025-03-31 13:13:21 +0530

    intial

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,10 +1,10 @@
 import Banner1 from '../assets/banner.png'
 export default function Banner(){
-    return <div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+    return <div className='py-14'>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 p-4">
             <div className='flex flex-col justify-center'><img src={Banner1} alt="banner1" className='mx-auto'/></div>
             <div className='space-y-4  flex justify-center flex-col items-center'>
-                <h1 className='text-3xl font-bold md:text-left'>We Belive that any team makes Project better</h1>
+                <h1 className='text-4xl font-bold font-serif md:text-left'>We Belive that any team makes Project better</h1>
                 <p className='text-gray-500 text-sm'>Lorem ipsum dolor sit amet, consectetur adipiscing  Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit, elit,Lorem ipsum dolor sit amet, consectetur</p>
             <div >
                 <button className='primary_btn  bg-black text-white shadow-[5px_5px_0px_0px_#6c6c6c]'>Discover Now</button>
--- src/components/nxtbanner.jsx
+++ src/components/nxtbanner.jsx
@@ -1,10 +1,24 @@
 import Banners from '../assets/banner2.png'
 export default function Banner2(){
-    return <div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
-            <div className='space-y-4  flex justify-center flex-col items-center'>
-                <h1 className='text-3xl font-bold md:text-left'>Simple Way To Make Stylish Livning Room</h1>
+    return <div className='py-14'>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 ">
+            <div className='space-y-4  flex justify-center flex-col items-center p-4'>
+                <h1 className='text-4xl font-bold  font-serif md:text-left'>Simple Way To Make Stylish Livning Room</h1>
                 <p className='text-gray-500 text-sm'>Lorem ipsum dolor sit amet, consectetur adipiscing  Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit, elit,Lorem ipsum dolor sit amet, consectetur</p>
+            <div className='flex gap-3'>
+                <div className='max-w-[80px] space-y-2'>
+                    <p className='text-3xl font-bold font-serif'>15</p>
+                    <p className='text-gray-500'>Years of experience</p>
+                 </div>
+                 <div className='max-w-[80px] space-y-2'>
+                    <p className='text-3xl font-bold font-serif'>350</p>
+                    <p className='text-gray-500'>Happy Clients</p>
+                 </div>
+                 <div className='max-w-[80px] space-y-2'>
+                    <p className='text-3xl font-bold font-serif'>20</p>
+                    <p className='text-gray-500'>Awards</p>
+                 </div>
+            </div>
             <div >
                 <button className='primary_btn  bg-black text-white shadow-[5px_5px_0px_0px_#6c6c6c]'>Contact Us</button>
             </div>
